0048d12dc5ce3127b40ecda4787f4200ddf1c290
dhipgraby <kenneth.zambrano4@gmail.com>
2019-08-19 02:36:17 +0200

    first-empty-commit

--- head.php
+++
@@ -1,9 +0,0 @@
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title><?php echo $title;?></title>
-   <link rel="stylesheet"  href="css/bootstrap.min.css">
-	
-	<link rel="stylesheet" href="css/custom.css">
-	<link rel=icon href='img/logo-icon.png' sizes="32x32" type="image/png">
